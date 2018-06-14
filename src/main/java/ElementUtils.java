cb69550fffcc5fa5859f8cf919c424086328b290
Mateusz Olszewski <mocarny.olszak@gmail.com>
2018-06-14 17:22:25 +0200

    Add questions

--- src/main/java/ElementUtils.java
+++ src/main/java/ElementUtils.java
@@ -1,4 +1,13 @@
 public class ElementUtils {
+
+    // Używając typów generycznych zamień rozwiązania używające klasy String na takie, które będą mogły przyjąć
+    // dowolny element tj. zamień betterString na betterEntry i TwoStringPredicate na TwoElementPredicate. Teraz
+    // powinieneś mieć możliwość przekazania do metody betterElement dowolne elementy. Na przykład 2 samochody i
+    // predykat samochodowy, 2 pracowników i predykat pracowniczy, itd. :
+    //• ElementUtils.betterElement(string1, string2, (s1, s2) -> s1.length() > s2.length())
+    //• ElementUtils.betterElement(car1, car2, (c1, c2) -> c1.getPrice() > c2.getPrice())
+    //• ElementUtils.betterElement(employee1, employee2, (e1, e2) -> e1.getSalary() > e2.getSalary())
+
     public static <SomeType> SomeType betterElement(SomeType e1, SomeType e2, TwoElementPredicate<SomeType> predicate) {
         return predicate.verify(e1, e2) ? e1 : e2;
     }
--- src/main/java/LambdaExpressions.java
+++ src/main/java/LambdaExpressions.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LambdaExpressions {
+
+    // 1. Napisz metodę, która zwróci średnią z listy integerów.
     public static double average(List<Integer> list) {
         return list.stream()
                 .mapToInt(value -> value)
@@ -11,42 +13,53 @@ public class LambdaExpressions {
                 .orElse(0);
     }
 
+    // 2. Napisz metodę, która skonwertuje wszystkie Stringi z podanej listy, aby były zapisane wielkimi literami.
     public static List<String> upperCase(List<String> list) {
         return list.stream()
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
     }
 
+    // 3. Napisz metodę, która przyjmuje listę Stringów i zwróci listę wszystkich, które zaczynają się na „s” i mają
+    // dokładnie 4 litery.
     public static List<String> startsWithSAndHasFourLetters(List<String> list) {
         return list.stream()
                 .filter(element -> (element.startsWith("s") || element.startsWith("S")) && element.length() == 4)
                 .collect(Collectors.toList());
     }
 
+    // 4. Napisz metodę która, dostając listę integerów, zwróci Stringa z wartościami oddzielonymi przecinkami.
+    // Każdy element powinien być poprzedzony literą „p” jeśli jest parzysty, a literą „n” jeśli jest nieparzysty. Na
+    // przykład dla listy (3, 44) metoda powinna zwrócić „n3, p44”.
     public static String modifyAccordingToOddOrEven(List<Integer> list) {
         return list.stream()
                 .map(element -> element % 2 == 0 ? "p" + element : "n" + element)
                 .collect(Collectors.joining(", "));
     }
 
+    // 5. Stwórz metodę sortującą alfabetycznie listę stringów
     public static List<String> sortAlphabetically(List<String> list) {
         return list.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
 
+    // 6. Stwórz metodę sortującą listę stringów w kolejności odwrotnej do alfabetycznej
     public static List<String> sortReverseAlphabetically(List<String> list) {
         return list.stream()
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
     }
 
+    // 7. Stwórz metodę sortującą alfabetycznie listę stringów,  ale tylko według pierwszego znaku.
     public static List<String> sortOnlyAccordingToFirstChar(List<String> list) {
         return list.stream()
                 .sorted(Comparator.comparingInt(e -> e.charAt(0)))
                 .collect(Collectors.toList());
     }
 
+    // 8. Stwórz metodę sortującą, która posortuje Stringi tak, że na początku będą wyrazy zaczynające się na „e”, a
+    // dalej cała reszta. (Podpowiedź: lambda może zawierać nawiasy wąsate i słowo kluczowe return)
     public static List<String> sortIfStartsWithE(List<String> list) {
         return list.stream()
                 .sorted((e1, e2) -> {
